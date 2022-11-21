0e912e5e89019cebac95b7b32bc8a72ae61ea99c
Chris <chriszoelln@gmail.com>
2022-11-21 17:57:54 +0100

    Hero Section

--- components/Hero/Hero.jsx
+++ components/Hero/Hero.jsx
@@ -2,7 +2,19 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div>Hero hereeee</div>
+    <div className='flex flex-col justify-center items-center'>
+      <h1 className='text-white text-2xl font-bold'>Your Journal:</h1>
+
+      <div className='border-2 rounded-lg border-slate-600 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 my-5 px-10'>
+
+        <div className='p-3'>
+          <h2>Entry: at</h2>
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veritatis ab ad quidem, corporis saepe obcaecati, voluptas atque fugit quisquam ut quibusdam aut!</p>
+        </div>
+
+      </div>
+      
+    </div>
   )
 }
 
--- components/Navbar/Navbar.jsx
+++ components/Navbar/Navbar.jsx
@@ -5,14 +5,14 @@ import React from 'react'
 const Navbar = () => {
   return (
     <div>
-        <nav className='container mx-auto flex justify-between items-center'>
+        <nav className='container mx-auto flex justify-between items-center p-5'>
             <div className='flex justify-between items-center space-x-4'>
                 <h1 className='text-2xl text-white font-bold'>Web3 Journal</h1>
                 <Image className='' src={"/speech-bubble-white.png"} alt={"Logo"} width={30} height={30} />
             </div>
 
-            <div>
-                <h2>Address: <span>0xddfb6393b36rb</span></h2>
+            <div className='text-white rounded-full p-2 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500'>
+                <h2>Address: <span className='text-gray-800'>0xddfb6393b36rb</span></h2>
             </div>
         </nav>
     </div>
