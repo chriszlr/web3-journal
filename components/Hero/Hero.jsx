18138cf8cea344784458d85cce29ba32cd936d58
Chris <chriszoelln@gmail.com>
2022-11-21 18:01:41 +0100

    typo fix

--- components/Hero/Hero.jsx
+++ components/Hero/Hero.jsx
@@ -8,12 +8,11 @@ const Hero = () => {
       <div className='border-2 rounded-lg border-slate-600 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 my-5 px-10'>
 
         <div className='p-3'>
-          <h2>Entry: at</h2>
+          <h2>Entry at:</h2>
           <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veritatis ab ad quidem, corporis saepe obcaecati, voluptas atque fugit quisquam ut quibusdam aut!</p>
         </div>
 
       </div>
-      
     </div>
   )
 }
