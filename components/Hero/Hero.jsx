5efbf4b9e136663ec2b78b7e0b01b0ae0cd40239
Chris <chriszoelln@gmail.com>
2022-11-23 22:05:46 +0100

    error in Hero.jsx getEntries()

--- components/Hero/Hero.jsx
+++ components/Hero/Hero.jsx
@@ -1,10 +1,26 @@
 import React, { useState, useEffect } from 'react'
 import { ethers } from "ethers"
 import { NewEntry } from "../index"
+import { JournalAddress } from '../../config/constants'
+import { JournalABI } from '../../config/constants'
 
 const Hero = () => {
 
   const [NewEntryPopUp, setNewEntryPopUp] = useState(false)
+
+  useEffect(() => {
+    getEntries()
+  }, [])
+  
+
+  const getEntries = async () => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum)
+    const signer = provider.getSigner()
+    const JournalContract = new ethers.Contract(JournalAddress, JournalABI, signer)
+
+    const tx = await JournalContract.getCreatorData(signer)
+    console.log(tx)
+  }
 
   return (
     <div className='flex flex-col justify-center items-center'>
---
+++ config/constants.js
@@ -0,0 +1,4 @@
+import abi from "../artifacts/contracts/Journal.sol/Journal.json"
+
+export const JournalAddress="0x34f80E65499Ad4957139dbd5A63E0Ca6419f4E8a"
+export const JournalABI = abi.abi
